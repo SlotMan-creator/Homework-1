2fc4786498aab534ce6dbc11d1dee76cce45b095
SlotMan-creator <phoenix5812@yandex.ru>
2024-09-10 00:47:39 +0300

    Пока что сделал 10 заданий по js С 7м заданием недопонимание,в комментариях в кодеесть заметки

---
+++ hw-2.js
@@ -0,0 +1,98 @@
+// TaskOne
+
+let a = 10;
+alert(a)
+a = 20;
+alert(a);
+
+// TaskTwo
+
+let year = 2007;
+alert(year);
+
+// TaskThree
+
+let creatorName = String('Brendan');
+alert(creatorName);
+
+// Taskfour
+
+let numberTen = 10;
+let numberTwo = 2;
+let sum = numberTen + numberTwo;
+let difference = numberTen - numberTwo;
+let multiplication = numberTen * numberTwo;
+let division = numberTen / numberTwo;
+alert(sum);
+alert(difference);
+alert(multiplication);
+alert(division);
+
+// TaskFive
+
+let result = numberTwo **5;
+alert(result);
+
+// TaskSix
+
+let numberNine = 9;
+let remainder = numberNine % numberTwo;
+alert(remainder);
+
+
+// TaskSeven
+
+let num = 1;
+let sumOne = num +=5;
+let differenceOne = num -=3;
+let multiplicationOne = num *=7;
+let divisionOne = num /=3;
+let sumTwo = num++;
+let differenceTwo = num--;
+alert(sumOne);
+alert(differenceOne);
+alert(multiplicationOne);
+alert(divisionOne);
+alert(sumTwo);
+alert(differenceTwo);
+
+// Эту задачу смог только таким способом решить,
+// в консоли вообще пишет что переменная num уже объявлена,
+// а через обновление страницы работает,но
+
+// тут
+// let sumTwo = num++; Должно быть 8,тут 7
+// let differenceTwo = num--; должно быть 7,тут 8
+//  что-то  не сработало 
+
+// Кажется просто,но почему это вдруг так оказалось сложно,что не так я сделал
+
+
+
+// TaskEight
+
+let age = String(prompt('Сколько вам лет?'));
+alert(age);
+
+// TaskNine
+
+let name = String('Alexandr');
+let ageTwo = Number(20);
+let isAdmin = Boolean(true);
+
+const user = {
+    name: Alexandr,
+    ageTwo:20,
+    isAdmin: true
+}
+
+// TaskTen
+
+let nameOfUser = String(prompt('Как Вас зовут?'));
+alert(`Привет, ${nameOfUser}!`);
+
+
+
+
+
+
