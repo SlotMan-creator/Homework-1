784a75622745f9be9be3a08dadb593f05cf0d4d1
SlotMan-creator <phoenix5812@yandex.ru>
2024-09-13 08:45:19 +0300

    Чутка подкорректировал)

--- hw-2.js
+++ hw-2.js
@@ -12,7 +12,7 @@ alert(year);
 
 // TaskThree
 
-let creatorName = String('Brendan');
+let creatorName = 'Brendan';
 alert(creatorName);
 
 // Taskfour
@@ -43,42 +43,20 @@ alert(remainder);
 // TaskSeven
 
 let num = 1;
-let sumOne = num +=5;
-let differenceOne = num -=3;
-let multiplicationOne = num *=7;
-let divisionOne = num /=3;
-let sumTwo = num++;
-let differenceTwo = num--;
-alert(sumOne);
-alert(differenceOne);
-alert(multiplicationOne);
-alert(divisionOne);
-alert(sumTwo);
-alert(differenceTwo);
-
-// Эту задачу смог только таким способом решить,
-// в консоли вообще пишет что переменная num уже объявлена,
-// а через обновление страницы работает,но
-
-// тут
-// let sumTwo = num++; Должно быть 8,тут 7
-// let differenceTwo = num--; должно быть 7,тут 8
-//  что-то  не сработало 
-
-// Кажется просто,но почему это вдруг так оказалось сложно,что не так я сделал
-
-
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num)
 
 // TaskEight
 
-let age = String(prompt('Сколько вам лет?'));
+let age = prompt('Сколько вам лет?');
 alert(age);
 
 // TaskNine
-
-let name = String('Alexandr');
-let ageTwo = Number(20);
-let isAdmin = Boolean(true);
 
 const user = {
     name: 'Alexandr',
@@ -88,7 +66,7 @@ const user = {
 
 // TaskTen
 
-let nameOfUser = String(prompt('Как Вас зовут?'));
+let nameOfUser = prompt('Как Вас зовут?');
 alert(`Привет, ${nameOfUser}!`);
 
 
