89cdad6acf69d0b11be510b651137b2a3d436eca
SlotMan-creator <phoenix5812@yandex.ru>
2024-09-17 00:53:23 +0300

    Первые 3 задачи js готовы,нужен сон)

---
+++ hw-4.js
@@ -0,0 +1,23 @@
+// TaskOne
+
+let i = 0;
+do {
+    console.log('Привет!');
+    i++;
+} while(i <=1);
+
+// TaskTwo
+
+let d = 0;
+while (d < 5) {
+    d++;
+    console.log(d);
+}
+
+// TaskThree
+
+let s = 6;
+while (s < 22) {
+    s++;
+    console.log(s);
+}
