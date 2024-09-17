65a5a63961881eace44ea531daeb4aefb9d8c70f
SlotMan-creator <phoenix5812@yandex.ru>
2024-09-17 11:29:17 +0300

    дз готово

--- hw-4.js
+++ hw-4.js
@@ -4,7 +4,7 @@ let i = 0;
 do {
     console.log('Привет!');
     i++;
-} while(i <=1);
+} while (i <= 1);
 
 // TaskTwo
 
@@ -21,3 +21,44 @@ while (s < 22) {
     s++;
     console.log(s);
 }
+
+// TaskFour
+
+const obj = {
+    Коля: '200$',
+    Вася: '300$',
+    Петя: '400$'
+}
+
+for (const key in obj) {
+    console.log(`${key}: ${obj[key]}`);
+}
+
+// TaskFive
+
+let n = 1000;
+let num = 0
+while (n > 50) {
+    n /=2;
+    num++;
+    console.log(n);
+    console.log(num);
+}
+
+// TaskSix
+
+for (let numberFriday = 1; numberFriday <=5; numberFriday++) {
+    if (numberFriday ===1) {
+        console.log('Сегодня пятница,первое число.Необходимо подготовить отчет');
+    } else if (numberFriday ===2) {
+        console.log('Сегодня пятница,восьмое число.Необходимо подготовить отчет');
+    } else if (numberFriday ===3) {
+        console.log('Сегодня пятница,пятнадцатое число.Необходимо подготовить отчет');
+    } else if (numberFriday ===4) {
+        console.log('Сегодня пятница,двадцать второе число.Необходимо подготовить отчет');
+    } else if (numberFriday ===5) {
+        console.log('Сегодня пятница,двадцать девятое число.Необходимо подготовить отчет');
+    }
+}
+
+// Так ли я понял шестую задачу или же все-таки что-то здесь по другому?)
